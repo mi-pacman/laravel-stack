9f4e931f014409531dab30164864ed2eb7e7de9d
Ben Smith <ben@megacorp.industries>
2022-12-05 21:20:27 +0930

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -3,3 +3,5 @@ git clone https://github.com/laravel/laravel laravel
 sudo docker run --rm -v $(pwd)/laravel:/app composer install
 sudo chown -R dedsec:dedsec laravel
 sudo docker compose up -d
+sudo docker compose exec app php artisan key:generate
+sudo docker compose exec app php artisan config:cache
